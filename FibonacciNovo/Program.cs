9552b0c59a8db55542a63516de2b38336531d93f
Luis Guilherme <luislebert01@outlook.com>
2023-03-03 14:06:17 -0300

    Add project files.

---
+++ FibonacciNovo/Program.cs
@@ -0,0 +1,11 @@
+﻿int ant = 1, prox = 0;
+
+Console.WriteLine("Digite a quantidade de posições");
+int quantidade = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < quantidade; i++)
+{
+    Console.Write("{0} ", prox + ant);
+    prox = ant + prox;
+    ant = prox - ant;
+}
